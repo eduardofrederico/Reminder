3e146f16b03dabf3b2909f17b79a514789d0ff07
Eduardo Frederico <eduardo.frederico@icloud.com>
2026-04-08 20:43:11 -0300

    Entendendo e criando um BottomSheet.

--- Reminder/Sources/Features/Splash/SplashViewController.swift
+++ Reminder/Sources/Features/Splash/SplashViewController.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+import UIKit
+
+class SplashViewController: UIViewController {
+    let contentView = SplashView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupGesture()
+        
+        //ciclo de decisão se vai para login ou para menu
+        setup()
+        
+    }
+    
+    private func setup() {
+        self.view.addSubview(contentView)
+        self.navigationController?.navigationBar.isHidden = true
+        self.view.backgroundColor = Colors.primaryRedBase
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func setupGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showLoginBottomSheet))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func showLoginBottomSheet() {
+        let loginBottomSheet = LoginBottomSheetViewController()
+        loginBottomSheet.modalPresentationStyle = .overCurrentContext
+        loginBottomSheet.modalTransitionStyle = .crossDissolve
+        self.present(loginBottomSheet, animated: false) {
+            loginBottomSheet.animateShow()
+        }
+    }
+    
+}
