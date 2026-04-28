79e517e56feeb0b1a1e0d9606bd00c401de1a178
Eduardo Frederico <eduardo.frederico@icloud.com>
2026-04-28 00:13:55 -0300

    Iniciando a construção da Factory.

---
+++ Reminder/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactory.swift
@@ -0,0 +1,8 @@
+//
+//  ViewControllersFactory.swift
+//  Reminder
+//
+//  Created by Maria Eliza on 27/04/26.
+//
+
+import Foundation
---
+++ Reminder/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ViewControllersFactoryProtocol.swift
+//  Reminder
+//
+//  Created by Maria Eliza on 27/04/26.
+//
+
+import Foundation
