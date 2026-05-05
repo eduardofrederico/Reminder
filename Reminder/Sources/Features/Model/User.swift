d1aeb30e0ada985c42b930049fb0a76868387787
Eduardo Frederico <eduardo.frederico@icloud.com>
2026-05-04 21:42:45 -0300

    Criando UserDefaults.

---
+++ Reminder/Sources/Features/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Reminder
+//
+//  Created by Maria Eliza on 04/05/26.
+//
+
+import Foundation
---
+++ Reminder/Sources/Features/UserDefaults/UserDefaultsManager.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultsManager.swift
+//  Reminder
+//
+//  Created by Maria Eliza on 04/05/26.
+//
+
+import Foundation
